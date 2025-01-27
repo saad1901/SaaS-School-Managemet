401de462f3f2d16fe725c0292be9e137c7d05534
saadWindows <saadiqbal1921@gmail.com>
2025-01-27 16:38:32 +0530

    v3

--- saas_school_management/settings.py
+++ saas_school_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wo_j)axa(vrj&53gz!n+hnn8%3y4)$9+*e*j4za^1=pt*l+(xa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saad1921.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','saad1921.pythonanywhere.com']
 
 
 # Application definition
