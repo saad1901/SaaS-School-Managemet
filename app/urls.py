0e6cba40779d7c5840afab1c2e72c60eba46d3af
saadWindows <saadiqbal1921@gmail.com>
2025-01-29 15:46:29 +0530

    v6

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.login_view, name='login_view'),
-]
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
